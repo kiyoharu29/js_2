870dd887293d150c518793030595029cb7036b4e
EC2 Default User <ec2-user@ip-172-31-8-91.us-east-2.compute.internal>
2021-03-09 17:33:43 +0000

    電卓課題

---
+++ main2.js
@@ -0,0 +1,27 @@
+document.addEventListener('click',function(event){
+  
+  if (event.target.matches('#eq')) { 
+    var calcResult = eval(document.getElementById('display').innerText);
+    document.getElementById('display').innerText = calcResult;
+    //console.log(calcResult);
+    return; 
+  }
+  
+  if (event.target.matches('.clear')) { //trueの場合のみ実行
+    document.getElementById('display').innerText = ''; //空にする
+    return; //returnで返して終了させ、この下のコードは実行させない
+  }
+  
+  if (!event.target.matches('.number')) return;
+  
+  var clickNumber = event.target.innerText;
+  
+  var beforeClickNumber = document.getElementById('display').innerText;
+  
+  var x = 2*3;
+  //console.log(x);
+  
+  
+  
+  document.getElementById('display').innerText = beforeClickNumber + clickNumber; //表示結果をすべて表示させる
+})
